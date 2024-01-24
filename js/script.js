3dac3eaf6cd778d026d83c383b7a54418ae76d96
LukePadawalker <padurariu.luca@gmail.com>
2024-01-24 16:48:11 +0100

    trying to generate the contact list

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 console.log("vue ok", Vue)
 
-const { createApp} = Vue;
+const { createApp } = Vue;
+
+const data = data;
 
 const app = createApp({
-
+    data() {
+        data
+    }
 })
 
 app.mount("#root");
