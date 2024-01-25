e487f6e4ac587bb7534a818f4c6f3aff99e81a32
LukePadawalker <padurariu.luca@gmail.com>
2024-01-25 17:56:15 +0100

    milestone 2 almost done

--- js/script.js
+++ js/script.js
@@ -10,34 +10,53 @@ const app = createApp({
             contacts: data.contacts,
             currentUser: '',
             currentAvatar: '',
-            textMessages: '',
-            textStatus: '',
-            textDate: '',
-
+            textMessagesArray: [],
+            textId: [],
+            textMessages: [],
+            textStatus: [],
+            textDate: [],
+            chatSearchInput: [],
         }
     },
     methods: {
         showClickedContactChat(contactId, contactName, contactAvatar, contactMessages) {
-            console.log(contactId);
-            console.log(contactName);
-            console.log(contactAvatar);
-            console.log(contactMessages);
 
-            // this.currentUser = contactName;
-            // this.currentAvatar = contactAvatar;
-            // this.textMessages = contactMessages.text;
-            // console.log(textMessages)
-            // this.textStatus = messages.status;
-            // this.textDate = messages.date;
-            // console.log(textMessages);
-            // console.log(textStatus);
+            // show the contact info on right header
+            this.currentUser = contactName;
+            this.currentAvatar = contactAvatar;
+            this.textId = [];
+            this.textMessages = [];
+            this.textStatus =[];
+            this.textDate = [];
+
+            // devo linkare al v-for delle chat, l'array completo
+            
+            // show the messages 
+            this.textMessagesArray = contactMessages;
+            for (let i = 0; i < contactMessages.length; i++){
+                this.textId.push(contactMessages[i].id);
+                this.textStatus.push(contactMessages[i].status);
+                this.textDate.push(contactMessages[i].date);
+                this.textMessages.push(contactMessages[i].text);
+                
+            }
+
+            console.log(this.textId);
+            console.log(this.textDate);
+            console.log(this.textMessages);
+            console.log(this.textStatus);
+
+
+
+
+
+
+
+
+            // devi mostrare a schermo la chat
         }
     },
 
-    computed: {
-        chatInputSearch: '',
-
-    }
 })
 
 app.mount("#root");
